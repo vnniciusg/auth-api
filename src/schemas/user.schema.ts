64835668194569d99411a63757e0459ddc3d998d
vnniciusg <90412694+vnniciusg@users.noreply.github.com>
2023-05-26 15:23:36 -0300

    a

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -7,7 +7,8 @@ enum RoleEnumType {
 
 export const createUserSchema = object({
     body: object({
-        name:string({required_error:'Name is required'}),
+        firstname:string({required_error:'First name is required'}),
+        lastname:string({required_error:'Last name is required'}),
         email:string({required_error:'Email is required'}).email('Invalid email address'),
         password:string({required_error:'Password is required'})
                                                                 .min(8,'Password must be more than 8 characters')
